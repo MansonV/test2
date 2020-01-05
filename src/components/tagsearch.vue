3157a274660906c41a7a1b9f2aefd73f556d8b4c
MansonV <59490357+MansonV@users.noreply.github.com>
2020-01-05 02:41:35 +0100

    Update tagsearch.vue

--- src/components/tagsearch.vue
+++ src/components/tagsearch.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container-fluid">
     <div class="search-item">
-    <!-- the search bar form -->
       <form v-on:submit="getfilteredData">
         <div class="form-row">
           <div class="col-10">
@@ -93,12 +92,10 @@ export default {
       this.filteredData = data;
       let filteredDataByfilters = [];
       let filteredDataBySearch = [];
-      // first check if filters where selected
       if (this.selectedFilters.length > 0) {
         filteredDataByfilters= this.filteredData.filter(obj => this.selectedFilters.every(val => obj.stack.indexOf(val) >= 0));
         this.filteredData = filteredDataByfilters;
       }
-      // then filter according to keyword, for now this only affects the name attribute of each data
       if (this.search !== '') {
         filteredDataBySearch = this.filteredData.filter(obj => obj.name.indexOf(this.search.toLowerCase()) >= 0);
         this.filteredData = filteredDataBySearch;
